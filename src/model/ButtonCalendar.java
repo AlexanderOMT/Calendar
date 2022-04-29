a870e327197a1785cfccf3f434daf3a55397af2f
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-04-29 13:17:10 +0100

    createCorreoInvitationTitle modificado

--- src/model/ButtonCalendar.java
+++ src/model/ButtonCalendar.java
@@ -5,11 +5,12 @@
  */
 package model;
 
-import java.awt.Color;
-import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -74,14 +75,30 @@ public class ButtonCalendar {
         return botonAux;
     }
     
+    public JButton createButtonNotification(int posicionCalendarioxAux, int posicionCalendarioyAux){
+        JButton botonAux = new JButton();
+        botonAux.setSize(75,40);
+        
+        botonAux.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
+        return botonAux;
+    }
+    
+    public JScrollPane createTextFieldNotification(int posicionCalendarioxAux, int posicionCalendarioyAux, String texto){
+        JTextField textoaux = new JTextField(texto);
+        textoaux.setFont(new java.awt.Font("asd", 0, 15)); // NOI18N
+        textoaux.setEditable(false);
+        JScrollPane textoroll= new JScrollPane(textoaux);
+        textoroll.setSize(535,50);
+        textoroll.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
+        return textoroll;
+    }
+    
     public JButton createButtonPrincipal(int posicionCalendarioxAux, int posicionCalendarioyAux){
         JButton botonAux = new JButton();
         botonAux.setSize(233, 169);
         botonAux.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
         botonAux.setFont(new java.awt.Font("Rockwell", 0, 20));
         botonAux.setText("+");
-        
-        botonAux.setBackground(new Color(203,239,255));
         return botonAux;
     }
     
@@ -104,4 +121,38 @@ public class ButtonCalendar {
         eliminarAux.setLocation(posicionCalendarioxAux+160,posicionCalendarioyAux-39);
         return eliminarAux;
     }
+    //ZONA INVITATION SHARE
+    public JLabel createCorreoInvitationTitle(String correo, int posicionCalendarioxAux, int posicionCalendarioyAux){
+        JLabel tituloAux = new JLabel();
+        tituloAux.setSize(225, 25);
+        tituloAux.setText(correo);
+        tituloAux.setBorder(null);
+        tituloAux.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
+        return tituloAux;
+    }
+    
+     public JLabel createCorreoInvitationRol(String rol, int posicionCalendarioxAux, int posicionCalendarioyAux){
+        JLabel tituloAux = new JLabel();
+        tituloAux.setSize(85, 25);
+        tituloAux.setText(rol);
+        tituloAux.setBorder(null);
+        tituloAux.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
+        return tituloAux;
+    }
+    public JButton createButtonInvitation(int posicionCalendarioxAux, int posicionCalendarioyAux){
+        JButton botonAux = new JButton();
+        botonAux.setSize(85, 25);
+        
+        botonAux.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
+        return botonAux;
+    }
+    public JComboBox createJComboBox(int posicionCalendarioxAux, int posicionCalendarioyAux){
+        javax.swing.JComboBox<String> botonAux=new javax.swing.JComboBox<>();
+        botonAux.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Editor", "Lector"}));
+        botonAux.setSize(85, 25);
+        
+        botonAux.setLocation(posicionCalendarioxAux,posicionCalendarioyAux);
+        return botonAux;
+    }
+
 }
