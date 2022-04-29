cac32855068dfbba6ec1b88f4d3ea0efe9881fcd
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-04-29 13:18:29 +0100

    Update Task.java

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,22 +1,72 @@
 package model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.sql.Timestamp;
 
 public class Task {
     
+    private int id;
     private String name;
     private String desc;
-    private String hour;
+    private Timestamp date;
     private int prior;
+    private String tag;
+    private String specialId;
     
     public Task () {
     }
-   
-    public Task (String name, String desc, String hour, int prior) {
+    
+    public Task (int id, String name, String desc, Timestamp date, int prior, String tag) {
+        this.id = id;
         this.name = name;
         this.desc = desc;
-        this.hour = hour;
+        this.date = date;
         this.prior = prior;
+        this.tag = tag;
+    }
+    
+    public Task (String name, String desc, Timestamp date, int prior, String tag, String specialId) {
+        this.name = name;
+        this.desc = desc;
+        this.date = date;
+        this.prior = prior;
+        this.tag = tag;
+        this.specialId = specialId;
+    }
+    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setId(String specialId) {
+        this.specialId = specialId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public void setPrior(int prior) {
+        this.prior = prior;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getSpecialId() {
+        return specialId;
     }
     
     public String getName() {
@@ -27,11 +77,15 @@ public class Task {
         return this.desc;
     }
     
-    public String getHour() {
-        return this.hour;
+    public Timestamp getDate() {
+        return this.date;
     }
     
     public int getPrior() {
         return this.prior;
+    }
+    
+    public String getTag() {        
+        return tag;
     }
 }
